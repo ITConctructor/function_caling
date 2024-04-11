07293ce245179d56c2075b14776ab63858b1846e
Евгений Маслов <evgeniy.maslov@etalongroup.com>
2024-04-11 15:53:02 +0300

    Add saiga_ru

--- src/utils/data/formats/__init__.py
+++ src/utils/data/formats/__init__.py
@@ -6,6 +6,12 @@ from .llama import (
     TOOL_CALL_B as LLAMA_TOOL_CALL_B,
     TOOL_CALL_E as LLAMA_TOOL_CALL_E,
 )
+from .saiga_ru import (
+    create_saiga_prompt,
+    create_saiga_test_prompt,
+    TOOL_CALL_B as SAIGA_TOOL_CALL_B,
+    TOOL_CALL_E as SAIGA_TOOL_CALL_E,
+)
 
 FORMATS_DICT = {
     "raw": {
@@ -19,5 +25,11 @@ FORMATS_DICT = {
         "test": create_llama_test_prompt,
         "tool_call_b": LLAMA_TOOL_CALL_B,
         "tool_call_e": LLAMA_TOOL_CALL_E,
+    },
+    "saiga_ru": {
+        "train": create_saiga_prompt,
+        "test": create_saiga_test_prompt,
+        "tool_call_b": SAIGA_TOOL_CALL_B,
+        "tool_call_e": SAIGA_TOOL_CALL_E,
     },
 }
